190a3d015d1ca92ac8eba40669230ad9865857ae
taynasuperti <taynasuperti@gmail.com>
2025-01-02 22:55:23 -0300

    adiciona interação de ícone de play e ajusta layout de faixas populares

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 // Controle de Reprodução e Aleatório
-document.querySelector('.play-button').addEventListener('click', function() {
+document.querySelector('.play-icon').addEventListener('click', function() {
     console.log('Reproduzindo música...');
-    // Adicione a lógica de reprodução aqui
 });
 
-document.querySelector('.random-button').addEventListener('click', function() {
-    console.log('Modo aleatório ativado...');
-    // Adicione a lógica para o modo aleatório aqui
+const shuffleButton = document.querySelector('.shuffle-icon');
+
+shuffleButton.addEventListener('click', function() {
+    shuffleButton.classList.toggle('active');
+    console.log('Modo aleatório ' + (shuffleButton.classList.contains('active') ? 'ativado' : 'desativado'));
 });
 
-// Seguir o artista (sem funcionalidade no momento)
-document.querySelector('.follow-button').addEventListener('click', function() {
-    console.log('Seguindo o artista...');
+
+// Seguir o artista 
+const followButton = document.querySelector('.follow-button');
+
+followButton.addEventListener('click', function () {
+    if (followButton.innerText === 'Seguir') {
+        followButton.innerText = 'Seguindo';
+        followButton.classList.add('active');
+    } else {
+        followButton.innerText = 'Seguir';
+        followButton.classList.remove('active');
+    }
+    console.log(followButton); // Verifica se o botão foi encontrado
+
 });
+
+
+// Lógica para alternar o ícone de "curtir" e "descurtir"
+const likeIcons = document.querySelectorAll('.like-icon i'); // Seleciona todos os ícones de "curtir"
+
+likeIcons.forEach(icon => {
+    icon.addEventListener('click', function() {
+        if (icon.classList.contains('fa-heart')) {
+            icon.classList.remove('fa-heart');
+            icon.classList.add('fa-solid', 'fa-heart', 'liked');
+            console.log('Música curtida!');
+        } else {
+            icon.classList.remove('fa-solid', 'fa-heart', 'liked');
+            icon.classList.add('fa-heart');
+            console.log('Música descurtida!');
+        }
+    });
+});
+
